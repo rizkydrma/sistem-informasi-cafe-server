5b1c9ec1a2c38b442ee205c572ceed8c2f6c9223
unknown <rizky.darmarazak@gmail.com>
2021-08-29 21:19:47 +0700

    Fitur filter by category & tags also make it populate when method get

--- app/product/controller.js
+++ app/product/controller.js
@@ -7,11 +7,37 @@ const config = require('../config');
 
 async function index(req, res, next) {
   try {
-    let { limit, skip } = req.query;
+    let { limit, skip, q = '', category = '', tags = [] } = req.query;
 
-    let products = await Product.find()
+    let criteria = {};
+
+    if (q.length) {
+      criteria = {
+        ...criteria,
+        name: { $regex: `${q}`, $options: 'i' },
+      };
+    }
+
+    if (category.length) {
+      category = await Category.findOne({
+        name: { $regex: `${category}`, $options: 'i' },
+      });
+
+      if (category) {
+        criteria = { ...criteria, category: category._id };
+      }
+    }
+
+    if (tags.length) {
+      tags = await Tag.find({ name: { $in: tags } });
+      criteria = { ...criteria, tags: { $in: tags.map((tag) => tag._id) } };
+    }
+
+    let products = await Product.find(criteria)
       .limit(parseInt(limit))
-      .skip(parseInt(skip));
+      .skip(parseInt(skip))
+      .populate('category')
+      .populate('tags');
     return res.json(products);
   } catch (err) {
     next(err);
