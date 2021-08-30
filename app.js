34c4f4bf4ab272797e924d657109d3387b3a4f5a
unknown <rizky.darmarazak@gmail.com>
2021-08-30 17:20:01 +0700

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 const productRouter = require('./app/product/router');
 const categoryRouter = require('./app/category/router');
@@ -26,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use(decodeToken());
 
 app.use('/auth', authRouter);
